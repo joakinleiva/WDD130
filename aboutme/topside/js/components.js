0217b6d63ed71a96cdb3c3fdbf38cf5cab99f4df
joakinleiva <joako.leiva9@gmail.com>
2022-07-12 02:46:39 -0400

    week 12

--- aboutme/topside/js/components.js
+++ aboutme/topside/js/components.js
@@ -1,12 +1,26 @@
+// CONTACT
+
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+  .split("")
+  .map(
+    (letter, idx) =>
+    <span style="transition-delay:$(idx * 50)ms">$(letter)</span>
+)
+.join("");  
+});
+
+// SLIDER
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -15,33 +29,17 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-} 
-
-
-// -------------------------------
+}
 
 
 
-$(document).ready(function(){ 
-  $(window).scroll(function(){ 
-      if ($(this).scrollTop() > 3000) { 
-          $('#scroll').fadeIn(); 
-      } else { 
-          $('#scroll').fadeOut(); 
-      } 
-  }); 
-  $('#scroll').click(function(){ 
-      $("html, body").animate({ scrollTop: 0 }, 40); 
-      return false; 
-  }); 
-});
