8a922cfdb668e6341724036af4f8ae6f8d1ee264
joakinleiva <joako.leiva9@gmail.com>
2022-07-14 01:28:01 -0400

    week 13 done

--- aboutme/topside/js/components.js
+++ aboutme/topside/js/components.js
@@ -14,32 +14,53 @@ labels.forEach((label) => {
 
 // SLIDER
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-
+$(function() {
+				
+  var Page = (function() {
+    
+    var $navArrows = $( '#nav-arrows' ).hide(),
+        $shadow = $( '#shadow' ).hide(),
+        slicebox = $( '#sb-slider' ).slicebox( {
+          onReady : function() {
+            
+            $navArrows.show();
+            $shadow.show();
+            
+          },
+          orientation : 'r',
+          cuboidsRandom : true,
+          disperseFactor : 30
+        } ),
+        
+        init = function() {
+          
+          initEvents();
+          
+        },
+        initEvents = function() {
+          
+          // add navigation events
+          $navArrows.children( ':first' ).on( 'click', function() {
+            
+            slicebox.next();
+            return false;
+            
+          } );
+          
+          $navArrows.children( ':last' ).on( 'click', function() {
+            
+            slicebox.previous();
+            return false;
+            
+          } );
+          
+        };
+    
+    return { init : init };
+    
+  })();
+  
+  Page.init();
+  
+});
 
